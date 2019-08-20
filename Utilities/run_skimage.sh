3dd5e3736ea73f8956549b0b9ba2bb30e5475af9
Nick Stelzenmuller <nickstelzenmuller@gmail.com>
2019-08-20 12:00:07 +0200

    Production code updates

---
+++ Utilities/run_skimage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Add docker to authorized xhost list
+xhost +local:docker
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+
+# Set access to GUI
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+# Start docker container and run skimage
+docker run \
+    --net=host \
+    --ipc=host \
+    -e DISPLAY=$DISPLAY \
+    -e TZ="Europe/Paris" \
+    -v $XSOCK:$XSOCK \
+    -v $XAUTH:$XAUTH \
+    -e XAUTHORITY=$XAUTH \
+    -v /home/odroid/skimage_edge_deployment:/home \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/timezone:/etc/timezone:ro \
+    --rm \
+    nickstelzenmuller/skimage:ARM_prod \
+    python3 \
+    skimage_edge.py
+
+xhost -local:docker
+
