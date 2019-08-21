67135cde22b3ec5d0519d4ced2566f98e4a94a37
Nick Stelzenmuller <nickstelzenmuller@gmail.com>
2019-08-21 16:52:37 +0200

    Production code updates

--- Utilities/watchdog.sh
+++ Utilities/watchdog.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # docker-compose start_watchdog
 echo "hello"
 # Setup functions to monitor semphore directory and run skimage
-# function monitor_semaphore {
-#   inotifywait \
-#         -e modify,create \
-#         ${semaphore_dir}
-# }
+function monitor_semaphore {
+  inotifywait \
+        -e modify,create \
+        ${semaphore_dir}
+}
 
-# # run skimage, then restart skimage if there are any changes to the semaphore file
-# xhost +local:root && \
-#     docker-compose \
-#     -f /home/odroid/skimage_edge_deployment/Utilities/docker-compose.yml \
-#     prod_ARM bash
+# run skimage, then restart skimage if there are any changes to the semaphore file
+xhost +local:root && \
+    docker-compose \
+    -f /home/odroid/skimage_edge_deployment/Utilities/docker-compose.yml \
+    prod_ARM bash
 
-# # Start monitoring the semphore file
-# semaphore_dir="/home/odroid/skimage_edge_deployment/data/semaphore"
-# mkdir -p ${semaphore_dir}
-# echo "Monitoring semaphore file $semaphore_dir"
+# Start monitoring the semphore file
+semaphore_dir="/home/odroid/skimage_edge_deployment/data/semaphore"
+mkdir -p ${semaphore_dir}
+echo "Monitoring semaphore file $semaphore_dir"
 
-# while monitor_semaphore
-# do
-#     echo "Semaphore received, restarting Skimage container"
-#     # docker-compose start_skimage
-# done
+while monitor_semaphore
+do
+    echo "Semaphore received, restarting Skimage container"
+    # docker-compose start_skimage
+done
 
